84d4459de107f7ca5a71d59ce2ee613786ba29d5
Nicolas Ojeda Bar <no263@dpmms.cam.ac.uk>
2013-07-30 22:14:06 +0100

    Added free-variable and free-call computation of ANF IL.
    This will be used in the lambda lifter. The free-call computation
    collects the set of calls to user defined procedures that are
    'free' (not found in the expression by a LET_REC).

--- anf.ml
+++ anf.ml
@@ -49,3 +49,65 @@ type prog = {
   prog_strings : (Id.t * string) list;
   prog_body : exp
 }
+
+module S = Set.Make (Id)
+
+let remove_list l s =
+  List.fold_right S.remove l s
+
+let fv_value = function
+  | VUNDEF
+  | VINT _
+  | VNULL _ -> S.empty
+  | VVAR x
+  | VLOAD x -> S.singleton x
+
+let fv_cexp = function
+  | VAL v -> fv_value v
+  | ALLOCA _
+  | MALLOC _ -> S.empty
+  | ARRAY_MALLOC (v1, v2) -> S.union (fv_value v1) (fv_value v2)
+  | LOAD v -> fv_value v
+  | STORE (v1, v2)
+  | BINOP (v1, _, v2) -> S.union (fv_value v1) (fv_value v2)
+  | CALL (_, vs)
+  | EXT_CALL (_, vs) -> List.fold_left S.union S.empty (List.map fv_value vs)
+  | GEP (v, vs) -> List.fold_left S.union S.empty (List.map fv_value (v :: vs))
+  | PTRTOINT v -> fv_value v
+
+let rec fv = function
+  | DIE _ -> S.empty
+  | LET (x, _, e1, e2) ->
+      S.union (fv_cexp e1) (S.remove x (fv e2))
+  | IF (v, e1, e2) ->
+      S.union (fv e1) (S.union (fv e2) (fv_value v))
+  | RETURN c ->
+      fv_cexp c
+  | LET_BLOCK (_, args, e1, e2) ->
+      S.union (fv e2) (remove_list (List.map fst args) (fv e1))
+  | GOTO (_, vs) ->
+      List.fold_left S.union S.empty (List.map fv_value vs)
+  | LET_REC (fundefs, e) ->
+      S.union (fv e)
+        (List.fold_left (fun s fundef ->
+          S.union s (remove_list (List.map fst fundef.fn_args)
+            (fv fundef.fn_body))) S.empty fundefs)
+
+let rec fc = function
+  | DIE _ -> S.empty
+  | LET (_, _, CALL (x, _), e2) ->
+      S.add x (fc e2)
+  | LET (_, _, _, e2) ->
+      fc e2
+  | IF (_, e1, e2) ->
+      S.union (fc e1) (fc e2)
+  | RETURN (CALL (x, _)) ->
+      S.singleton x
+  | RETURN _ ->
+      S.empty
+  | LET_BLOCK (_, _, e1, e2) ->
+      S.union (fc e1) (fc e2)
+  | GOTO _ ->
+      S.empty
+  | LET_REC (fundefs, e) ->
+      remove_list (List.map (fun fundef -> fundef.fn_name) fundefs) (fc e)
