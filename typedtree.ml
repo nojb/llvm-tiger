17e1a7de165bad0441afd062bed9665de13c8053
Nicolas Ojeda Bar <no263@dpmms.cam.ac.uk>
2013-08-01 16:28:33 +0100

    Forgot to add Typedtree to last commit.
    This is the output of the typechecking phase.

---
+++ typedtree.ml
@@ -0,0 +1,40 @@
+open Globals
+
+type type_spec =
+  | VOID
+  | INT
+  | STRING
+  | ARRAY   of string * type_spec
+  | RECORD  of string * Id.t (* name, unique name *)
+  | PLACE   of string
+
+type bin =
+  Parsetree.bin
+
+type access =
+  | Local
+  | NonLocal of int
+
+type var =
+  | TVlocal of string
+  | TVnonLocal of int * int
+  | TVsubscript of int * var * exp
+  | TVfield of int * var * int
+
+and exp =
+  | Tint of int
+  | Tstring of string
+  | Tnil of type_spec
+  | Tvar of var
+  | Tbinop of exp * bin * exp
+  | Tassign of var * exp
+  | Tcall of string * exp list
+  | Tseq of exp list
+  | Tmakearray of exp * exp
+  | Tmakerecord of string * exp list
+  | Tif of exp * exp * exp
+  | Twhile of exp * exp
+  | Tfor of string * exp * exp * exp
+  | Tbreak
+  | Tletvar of string * access ref * exp * exp
+  | Tletfuns of (string, type_spec, type_spec * access ref, exp) fundef list * exp
